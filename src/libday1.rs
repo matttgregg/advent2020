22be30cf30eb0087d410b44adb097d33c7744d86
Matthew Gregg <m.gregg@synthace.com>
2020-12-01 18:05:54 +0000

    Fiddle with day 1

--- src/libday1.rs
+++ src/libday1.rs
@@ -9,7 +9,8 @@ pub struct Config {
 
 impl Config {
     pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
-        args.next();
+        args.next(); // Skip filename
+        args.next(); // Skip day index
 
         let filename = match args.next() {
             Some(arg) => arg,
@@ -23,26 +24,68 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
     let contents: HashSet<i32> = contents.lines().map(|x| x.parse::<i32>().unwrap_or(0)).collect();
+    println!("Scanning {} entries.", contents.len());
 
     // Find sums to 2020
-    for val in contents.iter() {
-        let complement = 2020 - val;
-        if val < &complement && contents.contains(&complement) {
-            println!("{} x {} -> {}", val, complement, val * complement);
-        }
-
-        if val < &complement {
-            sums_to_part(&contents, &val);
-        }
+    let part1 = sums_to(&contents, 2020);
+    for (a,b) in part1.iter() {
+        println!("{} x {} -> {}", a, b, a*b);
     }
+
+    let part2 = triple_sums_to(&contents, 2020);
+    for (a,b,c) in part2.iter() {
+        println!("{} x {} x {} -> {}", a, b, c, a*b*c);
+    }
+
     Ok(())
 }
 
-fn sums_to_part(vals: &HashSet<i32>, part: &i32) {
+fn sums_to(vals: &HashSet<i32>, total: i32) -> Vec<(i32, i32)> {
+    let mut results: Vec<(i32, i32)> = Vec::new();
     for val in vals.iter() {
-        let complement = 2020 - val - part;
-        if part < val && val < &complement && vals.contains(&complement) {
-            println!("{} x {} x {} -> {}", part, val, complement, part * val * complement);
+        let complement = total - val;
+        if val < &complement && vals.contains(&complement) {
+            results.push((*val, complement));
         }
     }
+    results
 }
+
+fn triple_sums_to(vals: &HashSet<i32>, total: i32) -> Vec<(i32, i32, i32)> {
+    let mut results: Vec<(i32, i32, i32)> = Vec::new();
+    for val in vals.iter() {
+        let complement = total - val;
+        if val < &complement {
+            let triples = sums_to(vals, complement);
+            for (a,b) in triples.iter() {
+                if val < a {
+                    results.push((*val, *a, *b));
+                }
+            }
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn part1_test() {
+        let data = vec![1721, 979, 366, 299, 675, 1456];
+        let data: HashSet<i32> = data.into_iter().collect();
+        let res = sums_to(&data, 2020);
+        assert_eq!(1, res.len());
+        assert_eq!((299,1721), res[0]);
+    }
+
+    #[test]
+    fn part2_test() {
+        let data = vec![1721, 979, 366, 299, 675, 1456];
+        let data: HashSet<i32> = data.into_iter().collect();
+        let res = triple_sums_to(&data, 2020);
+        assert_eq!(1, res.len());
+        assert_eq!((366, 675, 979), res[0]);
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,41 @@
 use std::env;
 use std::process;
 
-use advent2020::ConfigD1 as Config;
-use advent2020::run_d1 as run;
+use advent2020::ConfigD1;
+use advent2020::run_d1;
 
 fn main() {
+    match day_arg(env::args()) {
+        Some(1) => run1(),
+        Some(x) => { eprintln!("Unimplemented day {}", x); process::exit(1);},
+        None => { eprintln!("No valid day on command line {:#?}", env::args()); process::exit(1);},
+    }
+}
+
+fn run1() {
     println!("Day1!");
-    let config = Config::new(env::args()).unwrap_or_else(|err| {
+    let config = ConfigD1::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    if let Err(e) = run(config) {
+    if let Err(e) = run_d1(config) {
         eprintln!("Error: {}", e);
         process::exit(1);
     }
+}
+
+fn day_arg(mut args: env::Args) -> Option<i32> {
+    args.next();
+
+    let day = match args.next() {
+        Some(day) => day.parse::<i32>(),
+        None => return None,
+    };
+
+    if let Ok(i) = day {
+        return Some(i);
+    }
+
+    return None
 }
