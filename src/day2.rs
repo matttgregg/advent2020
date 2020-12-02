c25814a006d6d0e3b94e347d8947b3221e91d497
Matthew Gregg <matt.t.gregg@gmail.com>
2020-12-02 10:59:57 +0000

    Day2 fiddling. Tests, functions, etc.

--- src/day2.rs
+++ src/day2.rs
@@ -38,6 +38,10 @@ impl RuledPassword {
         let mut got_min = false;
         let mut got_max = false;
         for (i, c) in self.password.char_indices() {
+            if i > self.max + 1 {
+                break;
+            }
+
             if c == self.letter {
                 if i + 1 == self.min { got_min = true; }
                 if i + 1 == self.max { got_max = true; }
@@ -50,14 +54,71 @@ impl RuledPassword {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     println!("Running day 2 on {}", config.filename);
-    let contents = fs::read_to_string(config.filename)?;
-    let contents = contents.lines();
-    let valid = contents.map(|x| RuledPassword::new(x)).filter(|x| x.as_ref().unwrap().valid2()).count();
-    println!("Found {} valid passwords.", valid);
-    let rp = RuledPassword::new("1-3 a: abcdeaaaa");
-    if let Some(p) = rp {
-    println!("{:?} {:?}", p, p.valid());
-    }
+    println!("Out of {} ...", line_count(&config.filename));
+    println!("Valid by method 1: {}", part1(&config.filename));
+    println!("Valid by method 2: {}", part2(&config.filename));
 
     Ok(())
+}
+
+fn part1(filename: &str) -> usize {
+    let contents = fs::read_to_string(filename);
+    if let Ok(c) = contents {
+        c.lines().filter_map(|x| RuledPassword::new(x)).filter(|x| x.valid()).count()
+    } else {
+        0
+    }
+}
+
+fn part2(filename: &str) -> usize {
+    let contents = fs::read_to_string(filename);
+    if let Ok(c) = contents {
+        c.lines().filter_map(|x| RuledPassword::new(x)).filter(|x| x.valid2()).count()
+    } else {
+        0
+    }
+}
+
+fn line_count(filename: &str) -> usize {
+    let contents = fs::read_to_string(filename);
+    if let Ok(c) = contents {
+        c.lines().count()
+    } else {
+        0
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_test() {
+        let d1 = RuledPassword::new("1-3 a: abcde").expect("Failed to parse.");
+        let d2 = RuledPassword::new("1-3 b: cdefg").expect("Failed to parse.");
+        let d3 = RuledPassword::new("2-9 c: ccccccccc").expect("Failed to parse.");
+
+        assert_eq!(d1.valid(), true);
+        assert_eq!(d2.valid(), false);
+        assert_eq!(d3.valid(), true);
+    }
+
+    #[test]
+    fn part2_test() {
+        let d1 = RuledPassword::new("1-3 a: abcde").expect("Failed to parse.");
+        let d2 = RuledPassword::new("1-3 b: cdefg").expect("Failed to parse.");
+        let d3 = RuledPassword::new("2-9 c: ccccccccc").expect("Failed to parse.");
+
+        assert_eq!(d1.valid2(), true);
+        assert_eq!(d2.valid2(), false);
+        assert_eq!(d3.valid2(), false);
+    }
+
+    #[test]
+    fn all_test() {
+        let file = "./data/data2.txt";
+        assert_eq!(538, part1(&file));
+        assert_eq!(489, part2(&file));
+    }
 }
