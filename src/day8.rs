fbf927d9b5ef37a6dfaae440c17d5169244d6c96
Matthew Gregg <matt.t.gregg@gmail.com>
2020-12-08 21:56:19 +0000

    Simplify instruction parsing for Day8.

--- src/day8.rs
+++ src/day8.rs
@@ -1,6 +1,3 @@
-use pest::Parser;
-use pest_derive::Parser;
-
 use std::collections::HashSet;
 use std::time::SystemTime;
 
@@ -25,7 +22,7 @@ pub fn run() {
 }
 
 fn solve_program(code: &str) -> (i64, i64) {
-    let prog = parse(code);
+    let prog = parse_fast(code);
 
     let loop_value = match run_program(&prog, false) {
         ProgramResult::Looped(acc) | ProgramResult::Terminated(acc) => acc,
@@ -35,42 +32,26 @@ fn solve_program(code: &str) -> (i64, i64) {
         ProgramResult::Terminated(fixed) => (loop_value, fixed),
         ProgramResult::Looped(_) => (loop_value, -1),
     }
-
 }
 
-fn parse(code: &str) -> Vec<Instruction> {
-    let parsed = DParser::parse(Rule::file, code)
-        .expect("could not parse data")
-        .next()
-        .unwrap();
-
+fn parse_fast(code: &str) -> Vec<Instruction> {
     let mut program = vec![];
 
-    for line in parsed.into_inner() {
-        if line.as_rule() == Rule::instruction {
-            let mut instr = line.into_inner();
-            
-            let op = instr.next();
-            let op = op.unwrap().as_str();
-            
-            let val = instr.next();
-            let val = val.unwrap().as_str();
-            let instruction = match op {
-                "nop" => {
-                    let (unpacked, sign) = parse_signed(val);
-                    Instruction::Nop(unpacked, sign)
-                },
-                "jmp" => {
-                    let (unpacked, sign) = parse_signed(val);
-                    Instruction::Jump(unpacked, sign)
-                },
-                "acc" => Instruction::Acc(val.parse::<i64>().unwrap()),
-                _ => Instruction::Ignored,
-            };
-            program.push(instruction);
-        }
+    for line in code.lines() {
+        let instruction = match &line[0..3] {
+            "nop" => {
+                let (unpacked, sign) = parse_signed(&line[4..]);
+                Instruction::Nop(unpacked, sign)
+            }
+            "jmp" => {
+                let (unpacked, sign) = parse_signed(&line[4..]);
+                Instruction::Jump(unpacked, sign)
+            }
+            "acc" => Instruction::Acc(line[4..].parse::<i64>().unwrap()),
+            _ => Instruction::Ignored,
+        };
+        program.push(instruction);
     }
-
     program
 }
 
@@ -78,7 +59,7 @@ fn parse_signed(v: &str) -> (usize, bool) {
     let sign = match v.chars().next() {
         Some('+') => true,
         Some('-') => false,
-        _ => panic!("Unexpected string format.")
+        _ => panic!("Unexpected string format."),
     };
     (v[1..].parse::<usize>().unwrap(), sign)
 }
@@ -95,72 +76,79 @@ enum Instruction {
     Ignored,
 }
 
-fn run_program(program: &[Instruction], flip: bool) -> ProgramResult  {
+fn run_program(program: &[Instruction], flip: bool) -> ProgramResult {
     let seen: HashSet<usize> = HashSet::new();
     let counter: usize = 0;
     let acc = 0;
     run_program_from(program, flip, &seen, counter, acc)
 }
 
-fn run_program_from(program: &[Instruction], flip: bool, init_seen: &HashSet<usize>, mut counter: usize, mut acc: i64) -> ProgramResult  {
-    // Need to copy the counter and accumulator.
+fn run_program_from(
+    program: &[Instruction],
+    flip: bool,
+    init_seen: &HashSet<usize>,
+    mut counter: usize,
+    mut acc: i64,
+) -> ProgramResult {
+    // Need to copy the loop breaker.
     let mut seen: HashSet<usize> = init_seen.iter().copied().collect();
 
     return loop {
         if seen.contains(&counter) {
-            break ProgramResult::Looped(acc)
+            break ProgramResult::Looped(acc);
         }
         seen.insert(counter);
 
         if let Some(ins) = program.get(counter) {
             match ins {
                 Instruction::Acc(val) => {
-                        acc += val;
-                        counter += 1;
+                    acc += val;
+                    counter += 1;
                 }
                 Instruction::Jump(val, sign) => {
                     if flip {
-                        // Try flipping to get a result.
-                        if let terminal@ProgramResult::Terminated(_) = run_program_from(program, false, &seen, counter + 1, acc) {
-                            break terminal
+                        // Try flipping to nop to get a result.
+                        if let terminal @ ProgramResult::Terminated(_) =
+                            run_program_from(program, false, &seen, counter + 1, acc)
+                        {
+                            break terminal;
                         }
                     }
- 
+
                     // Flip was unsuccesful, so continue.
                     if *sign {
                         counter = counter.saturating_add(*val);
                     } else {
                         counter = counter.saturating_sub(*val);
                     }
-                },
+                }
                 Instruction::Nop(val, sign) => {
                     if flip {
-                        // Try flipping to get a result.
-                        let shifted =
-                            if *sign {
-                                counter.saturating_add(*val)
-                            } else {
-                                counter.saturating_sub(*val)
-                            };
-                        if let terminal@ProgramResult::Terminated(_) = run_program_from(program, false, &seen, shifted, acc) {
-                            break terminal
+                        // Try flipping to jump to get a result.
+                        let shifted = if *sign {
+                            counter.saturating_add(*val)
+                        } else {
+                            counter.saturating_sub(*val)
+                        };
+                        if let terminal @ ProgramResult::Terminated(_) =
+                            run_program_from(program, false, &seen, shifted, acc)
+                        {
+                            break terminal;
                         }
                     }
+
+                    // Flip was unsuccesful, so continue.
                     counter += 1;
-                },
+                }
                 Instruction::Ignored => {
                     counter += 1;
                 }
             }
         } else {
-            return ProgramResult::Terminated(acc)
+            return ProgramResult::Terminated(acc);
         }
-    }
+    };
 }
-
-#[derive(Parser)]
-#[grammar = "parsers/day8.pest"]
-pub struct DParser {}
 
 mod tests {
     #[allow(unused_imports)]
