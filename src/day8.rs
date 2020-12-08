d40dd5de646fa4f58d20a57ac2abea7138af6ca3
Matthew Gregg <matt.t.gregg@gmail.com>
2020-12-08 17:11:31 +0000

    Tidy up for day 8. Fixed possible overflow error (i64 vs usize).

--- src/day8.rs
+++ src/day8.rs
@@ -27,22 +27,18 @@ pub fn run() {
 fn solve_program(code: &str) -> (i64, i64) {
     let prog = parse(code);
 
-    let loop_value = match run_program(&prog, 0) {
+    let loop_value = match run_program(&prog, false) {
         ProgramResult::Looped(acc) | ProgramResult::Terminated(acc) => acc,
     };
 
-    let mut fix_at = 1;
-    let fix_value = loop {
-        if let ProgramResult::Terminated(acc) = run_program(&prog, fix_at) {
-            break acc
-        }
-        fix_at += 1;
-    };
+    match run_program(&prog, true) {
+        ProgramResult::Terminated(fixed) => (loop_value, fixed),
+        ProgramResult::Looped(_) => (loop_value, -1),
+    }
 
-    (loop_value, fix_value)
 }
 
-fn parse(code: &str) -> Vec<(&str, i64)> {
+fn parse(code: &str) -> Vec<Instruction> {
     let parsed = DParser::parse(Rule::file, code)
         .expect("could not parse data")
         .next()
@@ -53,15 +49,38 @@ fn parse(code: &str) -> Vec<(&str, i64)> {
     for line in parsed.into_inner() {
         if line.as_rule() == Rule::instruction {
             let mut instr = line.into_inner();
+            
             let op = instr.next();
             let op = op.unwrap().as_str();
+            
             let val = instr.next();
-            let val = val.unwrap().as_str().parse::<i64>().unwrap_or(0);
-            program.push((op, val));
+            let val = val.unwrap().as_str();
+            let instruction = match op {
+                "nop" => {
+                    let (unpacked, sign) = parse_signed(val);
+                    Instruction::Nop(unpacked, sign)
+                },
+                "jmp" => {
+                    let (unpacked, sign) = parse_signed(val);
+                    Instruction::Jump(unpacked, sign)
+                },
+                "acc" => Instruction::Acc(val.parse::<i64>().unwrap()),
+                _ => Instruction::Ignored,
+            };
+            program.push(instruction);
         }
     }
 
     program
+}
+
+fn parse_signed(v: &str) -> (usize, bool) {
+    let sign = match v.chars().next() {
+        Some('+') => true,
+        Some('-') => false,
+        _ => panic!("Unexpected string format.")
+    };
+    (v[1..].parse::<usize>().unwrap(), sign)
 }
 
 enum ProgramResult {
@@ -69,51 +88,74 @@ enum ProgramResult {
     Terminated(i64),
 }
 
-fn run_program(program: &[(&str, i64)], mut flip_counter: usize) -> ProgramResult  {
-    let mut seen: HashSet<usize> = HashSet::new();
-    let mut counter: usize = 0;
-    let mut acc = 0;
+enum Instruction {
+    Jump(usize, bool),
+    Nop(usize, bool),
+    Acc(i64),
+    Ignored,
+}
 
-    loop {
+fn run_program(program: &[Instruction], flip: bool) -> ProgramResult  {
+    let seen: HashSet<usize> = HashSet::new();
+    let counter: usize = 0;
+    let acc = 0;
+    run_program_from(program, flip, &seen, counter, acc)
+}
+
+fn run_program_from(program: &[Instruction], flip: bool, init_seen: &HashSet<usize>, mut counter: usize, mut acc: i64) -> ProgramResult  {
+    // Need to copy the counter and accumulator.
+    let mut seen: HashSet<usize> = init_seen.iter().copied().collect();
+
+    return loop {
         if seen.contains(&counter) {
-            break;
+            break ProgramResult::Looped(acc)
         }
         seen.insert(counter);
 
-        let instr = program.get(counter);
-        if let Some((op, val)) = instr {
-            match *op {
-                "acc" => {
+        if let Some(ins) = program.get(counter) {
+            match ins {
+                Instruction::Acc(val) => {
                         acc += val;
                         counter += 1;
                 }
-                "jmp" => {
-                    if flip_counter == 1 {
-                        // Flip to a no-op.
-                        counter += 1;
-                    } else {
-                    counter += *val as usize;
+                Instruction::Jump(val, sign) => {
+                    if flip {
+                        // Try flipping to get a result.
+                        if let terminal@ProgramResult::Terminated(_) = run_program_from(program, false, &seen, counter + 1, acc) {
+                            break terminal
+                        }
                     }
-
-                    flip_counter = flip_counter.saturating_sub(1);
-                },
-                _ => {
-                    if flip_counter == 1 {
-                        // Flip to a jump
-                        counter += *val as usize;
+ 
+                    // Flip was unsuccesful, so continue.
+                    if *sign {
+                        counter = counter.saturating_add(*val);
                     } else {
-                        counter += 1;
+                        counter = counter.saturating_sub(*val);
                     }
-
-                    flip_counter = flip_counter.saturating_sub(1);
                 },
+                Instruction::Nop(val, sign) => {
+                    if flip {
+                        // Try flipping to get a result.
+                        let shifted =
+                            if *sign {
+                                counter.saturating_add(*val)
+                            } else {
+                                counter.saturating_sub(*val)
+                            };
+                        if let terminal@ProgramResult::Terminated(_) = run_program_from(program, false, &seen, shifted, acc) {
+                            break terminal
+                        }
+                    }
+                    counter += 1;
+                },
+                Instruction::Ignored => {
+                    counter += 1;
+                }
             }
         } else {
-            return ProgramResult::Terminated(acc);
+            return ProgramResult::Terminated(acc)
         }
     }
-
-    ProgramResult::Looped(acc)
 }
 
 #[derive(Parser)]
@@ -125,5 +167,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_all() {}
+    fn test_all() {
+        assert_eq!((1475, 1270), solve_program(data()));
+    }
 }
