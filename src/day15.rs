4b10a02ed2d6c44cf8a3e472392d55843f754009
Matthew Gregg <matt.t.gregg@gmail.com>
2020-12-15 14:00:56 +0000

    Try Day 15 with an array, 32 bit ints. Add some tests.

--- src/day15.rs
+++ src/day15.rs
@@ -1,5 +1,5 @@
 use std::time::SystemTime;
-use std::collections::HashMap;
+use std::convert::TryInto;
 
 use advent2020::{fmt_bright, print_day, print_duration};
 
@@ -11,7 +11,6 @@ pub fn run() {
     let start = SystemTime::now();
 
     // Let's do this...
-    //repeat_to(&[0,3,6], 2020);
     let res1 = repeat_to(&data, 2020);
     let res2 = repeat_to(&data, 30_000_000);
 
@@ -21,23 +20,22 @@ pub fn run() {
     print_duration(timed);
 }
 
-fn repeat_to(starting: &[u64], target: usize) -> u64 {
-    let mut seen = HashMap::new();
-    let mut last = 0;
+fn repeat_to(starting: &[u32], target: u32) -> u32 {
+    let mut last = 0_u32;
+    let mut cache = vec![u32::MAX; 30_000_000];
 
     for i in 0..target {
         let next;
-        if i < starting.len() {
-            next = starting[i];
-        } else if let Some(j) = seen.get(&last) {
-            // Have we seen the last number before?
-            next = (i - j) as u64;
-        } else {
+        if i < starting.len().try_into().unwrap() {
+            next = starting[i as usize];
+        } else if cache[last as usize] == u32::MAX {
             next = 0;
+        } else {
+            next = i - cache[last as usize];
         }
 
         // Make a note of the last number.
-        seen.insert(last, i);
+        cache[last as usize] = i;
         last = next;
     }
     last 
@@ -48,5 +46,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_all() {}
+    fn test_2020() {
+        let data = vec![11,0,1,10,5,19];
+        assert_eq!(870, repeat_to(&data, 2020));
+    }
+
+    #[test]
+    fn test_30mill() {
+        let data = vec![11,0,1,10,5,19];
+        assert_eq!(9136, repeat_to(&data, 30_000_000));
+    }
 }
