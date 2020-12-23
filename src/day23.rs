71e903f839149068e864f501da4d2474b97896fc
Matthew Gregg <matt.t.gregg@gmail.com>
2020-12-23 18:12:02 +0000

    Cleaned up the code.

--- src/day23.rs
+++ src/day23.rs
@@ -1,5 +1,5 @@
 use std::time::SystemTime;
-use std::collections::{HashSet, VecDeque};
+use std::collections::{HashSet};
 use std::convert::TryFrom;
 
 use advent2020::{fmt_bright, print_day, print_duration, crab};
@@ -14,21 +14,12 @@ pub fn run() {
     let start = SystemTime::now();
 
     // Let's do this...
-    let demo_data = "389125467";
-    let demo_cups_10 = play_until(&demo_data, 10);
-    let demo_cups_100 = play_until(&demo_data, 10);
-    
-    println!("Demo, after 10 = {}", result_for(&demo_cups_10));
-    println!("Demo, after 100 = {}", result_for(&demo_cups_100));
-    
-    //let demo_big = play_big_game(&demo_data, 10_000_000);
-    let demo_big = play_big_game(&demo_data, 1_000_000, 10_000_000);
-    println!("Finished big demo game.");
+    let first_game = play_game(data(), 100);
+    println!("Finished a suspiciously easy game with {}", fmt_bright(&first_game));
     let big_game = play_big_game(data(), 1_000_000, 10_000_000);
-    println!("Finally beaten that {} by finding my stars at {}", crab(), big_game);
-
 
     let timed = SystemTime::now().duration_since(start).unwrap();
+    println!("Finally beaten that {} by finding my stars at {}", crab(), fmt_bright(&big_game));
     print_duration(timed);
 }
 
@@ -37,7 +28,6 @@ fn play_big_game(init: &str, cups: usize, rounds: usize) -> u64 {
     for _ in 0..rounds {
         focus = smart_round(cups, &mut game, focus);
     }
-    //println!("Values from 1: {}", from_one(cups, &game));
 
     // Find left of one.
     let after_one = *game.get(1).unwrap();
@@ -47,8 +37,16 @@ fn play_big_game(init: &str, cups: usize, rounds: usize) -> u64 {
     println!("The values after 1 are: {} x {} ==> {}", after_one, next_after_one, grand_product);
     grand_product
 }
+fn play_game(init: &str, rounds: usize) -> String {
+    let cups = init.len();
+    let (mut game, mut focus) = init_smart_game(init, cups);
+    for _ in 0..rounds {
+        focus = smart_round(cups, &mut game, focus);
+    }
+    from_one(cups, &game)
+} 
 
-fn from_one(count: usize, cups: &Vec<usize>) -> String {
+fn from_one(count: usize, cups: &[usize]) -> String {
     let mut code = String::from("");
 
     let mut at_cup = 1;
@@ -128,86 +126,26 @@ fn init_smart_game(init: &str, max: usize) -> (Vec<usize>, usize) {
     (game, first)
 }
 
-fn result_for(game: &VecDeque<i32>) -> String {
-    let mut after_one = false;
-    let mut second_part = String::from("");
-    let mut first_part = String::from("");
-
-    for val in game {
-        if *val == 1 {
-            after_one = true;
-        } else if after_one {
-            first_part.push_str(format!("{}", val).as_str());
-        } else {
-            second_part.push_str(format!("{}", val).as_str());
-        }
-    }
-    format!("{}{}", first_part, second_part)
-}
-
-fn play_until(init: &str, rounds: usize) -> VecDeque<i32> {
-    let mut game = init_game(init);
-    for _ in 0..rounds {
-        game = play_round(game); 
-    }
-    game
-}
-
-fn init_game(init: &str) -> VecDeque<i32> {
-    let mut game = VecDeque::new();
-    for c in init.chars() {
-        if let Ok(val) = c.to_string().parse::<i32>() {
-            game.push_back(val);
-        }
-    }
-    game
-}
-
-
-fn play_round(mut game: VecDeque<i32>) -> VecDeque<i32> {
-    // Pop chars
-    let mut next_game = VecDeque::new();
-    let curr = game.pop_front().unwrap();
-    // Pop three to the side
-    let mut side = VecDeque::new();
-    let mut popped = HashSet::new();
-    for _ in 0..3 {
-        let pop = game.pop_front().unwrap();
-        popped.insert(pop);
-        side.push_back(pop);
-    }
-
-    // What's the next item?
-    let mut pop_target =  (curr - 2) % 9 + 1;
-    if pop_target < 1 {
-        pop_target += 9;
-    }
-    while popped.contains(&pop_target) {
-        pop_target = (pop_target - 2) % 9 + 1;
-        if pop_target < 1 {
-            pop_target += 9;
-        }
-    }
-
-    for v in game {
-        next_game.push_back(v);
-        if v == pop_target {
-            // Push the saved cards back in here.
-            for saved in &side {
-                next_game.push_back(*saved);
-            }
-        }
-    }
-
-    // Put the current value back at the end. (i.e. rotate clockwise)
-    next_game.push_back(curr);
-
-    next_game
-}
-
 mod tests {
     #[allow(unused_imports)]
     use super::*;
+
+    /*
+    #[test]
+    fn test_small() {
+        
+        let demo_data = "389125467";
+        let demo_cups_10 = play_until(&demo_data, 10);
+        let demo_cups_100 = play_until(&demo_data, 10);
+        
+        println!("Demo, after 10 = {}", result_for(&demo_cups_10));
+        println!("Demo, after 100 = {}", result_for(&demo_cups_100));
+        
+        //let demo_big = play_big_game(&demo_data, 10_000_000);
+        let demo_big = play_big_game(&demo_data, 1_000_000, 10_000_000);
+        println!("Finished big demo game.");
+    }
+    */
 
     #[test]
     fn test_all() {}
